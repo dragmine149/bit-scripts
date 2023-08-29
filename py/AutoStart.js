e65b38c5a53db0bc618e2d96bb50b82c1828ab96
dragmine <dan@thebanners.uk>
2023-08-29 14:44:42 +0100

    Version 0.1.2. Added autostart, updated builder with some improvements.

---
+++ py/AutoStart.js
@@ -0,0 +1,21 @@
+import {terminalInsert, addCallback, setCSS} from 'HTOP/console-doc.js';
+
+/** @param {NS} ns */
+export async function main(ns) {
+let html = `<span class="auto-main">`
+  html += `<span id="auto-alias" class="auto-alias">[Set recommended alias]</span>\n`;
+  html += `<span id="auto-help" class="auto-help">[Show help menu]</span>\n`;
+  html += `</span>`
+
+  let css = `<style id="pyauto">
+  .auto-main {white-space:pre; color:#ccc; font:20px monospace; line-height: 16px; text-align:right;}
+  .auto-alias {cursor:pointer; text-decoration:underline; color:#0ff;}
+  .auto-help {cursor:pointer; text-decoration:underline; color:#ff0;}
+  </style>`
+
+  setCSS("pyauto", css);
+  terminalInsert(html);
+
+  addCallback(".auto-alias", `alias py="run py/main.js"`);
+  addCallback(".auto-help", `run py/main.js --help`);
+}
